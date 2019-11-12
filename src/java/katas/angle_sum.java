00cdccd1502157543e885b54935163c3866f8a00
Anur Bašić <48890281+bashovski@users.noreply.github.com>
2019-11-12 03:12:21 +0100

    Added Angle Sum of n-polygon kata and test.

---
+++ src/java/katas/angle_sum.java
@@ -0,0 +1,9 @@
+/**
+    Kata link: https://www.codewars.com/kata/sum-of-angles/train/java
+ */
+
+public class AngleSum {
+  public static int sumOfAngles(int n) {
+    return (((n-2)*180));
+  }
+} 
